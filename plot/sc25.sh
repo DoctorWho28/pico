b5a72d95f1d0b356724f36186c439200329661a0
Daniele De Sensi <d.desensi.software@gmail.com>
2025-10-03 20:01:48 +0200

    Update figure generation in sc25.sh script

--- plot/sc25.sh
+++ plot/sc25.sh
@@ -8,33 +8,33 @@ echo "Generating boxplots ..."
 echo "Generating heatmaps ..."
 ./plot/heatmaps.sh &> /dev/null
 
-echo "Generating fig. 4 ..."
+echo "Generating fig. 5 ..."
 pushd tracer/sinfo
 python3 plot.py &> /dev/null
 popd
 
 mkdir -p plot/paper
 
-# Fig. 4
-cp tracer/sinfo/multi_box_min_None_allreduce_rabenseifner_vs_bine_bandwidth.pdf plot/paper/fig4.pdf
-
-# Fig. 7a
-cp plot/lumi/heatmaps/allreduce/tasks_per_node_1_metric_mean_base_all_y_no_False.pdf plot/paper/fig7a.pdf
-
-# Fig. 7b
-cp plot/lumi/boxplot.pdf plot/paper/fig7b.pdf
+# Fig. 5
+cp tracer/sinfo/multi_box_min_None_allreduce_rabenseifner_vs_bine_bandwidth.pdf plot/paper/fig5.pdf
 
 # Fig. 8a
-cp plot/leonardo/heatmaps/allreduce/tasks_per_node_1_metric_mean_base_all_y_no_False.pdf plot/paper/fig8a.pdf
+cp plot/lumi/heatmaps/allreduce/tasks_per_node_1_metric_mean_base_all_y_no_False.pdf plot/paper/fig8a.pdf
 
 # Fig. 8b
-cp plot/leonardo/boxplot.pdf plot/paper/fig8b.pdf
+cp plot/lumi/boxplot.pdf plot/paper/fig8b.pdf
 
 # Fig. 9a
-cp plot/mare_nostrum/boxplot.pdf plot/paper/fig9a.pdf
+cp plot/leonardo/heatmaps/allreduce/tasks_per_node_1_metric_mean_base_all_y_no_False.pdf plot/paper/fig9a.pdf
 
 # Fig. 9b
-cp plot/fugaku/boxplot.pdf plot/paper/fig9b.pdf
+cp plot/leonardo/boxplot.pdf plot/paper/fig9b.pdf
+
+# Fig. 10a
+cp plot/mare_nostrum/boxplot.pdf plot/paper/fig10a.pdf
+
+# Fig. 10b
+cp plot/fugaku/boxplot.pdf plot/paper/fig10b.pdf
 
 # Table 3
 cp plot/lumi/bine_vs_binomial.txt plot/paper/table3.txt
